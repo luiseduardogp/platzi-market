7dbeed195695750edc54daf064e030b8f37a0a17
Luis Eduardo Garcia Paredes <luisgarciaep@unimagdalena.edu.co>
2023-11-18 15:41:31 -0500

    estructura inicial del proyecto (Arquitectura)

--- src/main/java/com/platzi/market/PlatziMarketApplication.java
+++ src/main/java/com/platzi/market/PlatziMarketApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PlatziMarketApplication {
-
+	//dandole la arquitectura inicial al proyecto
 	public static void main(String[] args) {
 		SpringApplication.run(PlatziMarketApplication.class, args);
 	}
